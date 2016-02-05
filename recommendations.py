38f9a781709d249e6d788e3eaa871038eb3ed47a
smartins22 <sebasamartins@gmail.com>
2016-02-04 23:43:12 -0300

    adding reverse function

--- recommendations.py
+++ recommendations.py
@@ -118,4 +118,15 @@ def getRecommendations(prefs,person,similarity=sim_pearson):
 	rankings.reverse()
 	return rankings
 
-	
+#transform list of person in list of movies
+def transformPrefs(prefs):
+	result={}
+	for person in prefs:
+		for item in prefs[person]:
+			result.setdefault(item,{})
+			
+			#Flip item and person
+			result[item][person]=prefs[person][item]
+	return result
+
+
